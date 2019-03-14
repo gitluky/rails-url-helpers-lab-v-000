275d9216a8911f7038cdac8f4ba3865ecf3fce3c
Jonathan Luk <jluk87@gmail.com>
2019-03-14 18:59:29 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find_by_id(params[:id])
-    @student.activate = false
+    @student.active = false
   end
 
 
