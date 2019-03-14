d0bf966b6b7fef8e3d36c374072be2e243136fa2
Jonathan Luk <jluk87@gmail.com>
2019-03-14 18:58:46 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,18 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find_by_id(params[:id])
   end
+
+  def activate
+  end
+
+
 
   private
 
